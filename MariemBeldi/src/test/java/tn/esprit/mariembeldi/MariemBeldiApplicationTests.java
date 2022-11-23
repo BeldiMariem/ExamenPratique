89c8d2839ec275cc2f40254a0dc3b4734b765688
mariem beldi <beldi.mariem.1997@gmail.com>
2022-11-23 09:14:35 +0100

    fisrCommit

---
+++ MariemBeldi/src/test/java/tn/esprit/mariembeldi/MariemBeldiApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.esprit.mariembeldi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MariemBeldiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
